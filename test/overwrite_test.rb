7a25d47bc06915e1c5e593235a45f19d06cf086f
Ionatan Wiznia <ionatan.wiznia@traddia.com>
2013-02-07 13:23:25 +0100

    override test

--- test/overwrite_test.rb
+++ test/overwrite_test.rb
@@ -6,24 +6,20 @@ describe Configuration do
   before do
     # similar to config/sample d.rb
     Configuration.for('d'){
-      built_in_object_id Send('object_id')
-      object_id 42
       built_in_inspect Send('inspect')
       inspect 'forty-two'
-      p 42.0
+      id "anid"
+      send "love"
     }
 
     @c = Configuration.for 'd'
   end
 
   it "must overwrite built-in methods" do
-    @c.object_id.must_equal 42
-    @c.object_id.wont_equal @c.built_in_object_id
-
     @c.inspect.must_equal 'forty-two'
     @c.inspect.wont_equal @c.built_in_inspect
-
-    @c.p.must_equal 42.0
+    @c.id.must_equal 'anid'
+    @c.send.must_equal 'love'
   end
 
 end
